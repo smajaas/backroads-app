aa7f4d514a4cf7cf9c03fa75602de054a3d85e79
smajaas <smajaas@gmail.com>
2023-02-27 20:02:12 +0530

    sociallinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../images/logo.svg';
-import { pageLinks } from '../data';
+import { pageLinks, socialLinks } from '../data';
 
 const Navbar = () => {
   return (
@@ -24,35 +24,22 @@ const Navbar = () => {
             );
           })}
         </ul>
-
         <ul className='nav-icons'>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-facebook'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-twitter'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-squarespace'></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='nav-icon'
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
